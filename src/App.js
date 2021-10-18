f261c3b517f1234e7540633892ef0d48f81a9fc5
LitvE <yevgen.litvinov1980@gmail.com>
2021-10-18 21:04:47 +0300

    Components fragment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import Hello from './Components/Hello';
 
 function App() {
-  return <Hello name={'John'} surname={'Dow'}/>;
+  return (
+  <>
+  <Hello name={'John'} surname={'Dow'} />
+  <Hello name={'Ivan'} surname={'Ivanov'} />
+  <Hello name={'Petr'} surname={'Petrov'} />
+  </>
+  );
 }
 
 export default App;
