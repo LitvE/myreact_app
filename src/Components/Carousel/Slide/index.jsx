74fff3827603454282ae7b6b7cadddbe86b7b561
LitvE <yevgen.litvinov1980@gmail.com>
2021-11-03 22:38:58 +0200

    Carousel_HW_2

--- src/Components/Carousel/Slide/index.jsx
+++ src/Components/Carousel/Slide/index.jsx
@@ -2,35 +2,10 @@ import React, { Component } from 'react';
 import style from '../Slide/Slide.module.scss';
 
 class Slide extends Component {
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //        img,
-    //     };
-        
-    // }
-
-    // load = () => {
-    //     const {img} = this.state;
-    //     const {currentImage: {src}} = this.props;
-    //     const {currentImage: {alt}} = this.props;
-    //     const {currentImage: {width}} = this.props;
-    //     const {currentImage: {height}} = this.props;
-    //     img.src = src;
-    //     img.width = width;
-    //     img.height = height;
-    //     img.alt = alt;
-    //     this.setState({img: img});
-    // }
-
-    // componentDidMount () {
-    //     this.load();
-    // }
-
 
     render() {
         const {currentImage} = this.props;
-        
+
         return (
             <div className={style.slideStyle}>
                 <figure>
